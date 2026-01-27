35c112980bf5ef52960525ff1477ebd2873c01f8
Sebahstian Cadiz Resto <sebahstiancadiz1@outlook.com>
2026-01-27 15:37:58 -0600

    Implement mode calculation in mean_median_mode
    Added mode calculation to the mean_median_mode function.

--- mean-median-mode/mean-median-mode.py
+++ mean-median-mode/mean-median-mode.py
@@ -1,6 +1,6 @@
 import numpy as np
 from collections import Counter
-
+# List examples
 a = [1,2,2,3,3]
 
 def mean_median_mode(x):
@@ -10,9 +10,11 @@ def mean_median_mode(x):
     # Write code here
     mean = np.mean(x)
     median = np.median(x)
+    # Calculating the mode
     counts = Counter(x)
     max_counts = max(counts.values())
     modes = [val for val, count in counts.items() if count == max_counts]
     mode = float(min(modes))
+    
     return mean, median, mode
 
