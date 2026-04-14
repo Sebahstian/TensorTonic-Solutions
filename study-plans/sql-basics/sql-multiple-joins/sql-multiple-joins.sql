069613af6c27127b16c4c9b576ff13f3fcc9a29d
Sebahstian Cadiz Resto <sebahstiancadiz1@outlook.com>
2026-04-14 14:54:35 -0500

    Add solution for sql-multiple-joins

---
+++ study-plans/sql-basics/sql-multiple-joins/sql-multiple-joins.sql
@@ -0,0 +1,11 @@
+-- Write your SQL query here
+-- Tables: u=users, e=experiment_assignments, c=conversions
+select
+    u.username,
+    e.experiment_name,
+    e.variant,
+    c.revenue
+from users u
+join experiment_assignments e on u.id = e.user_id
+join conversions c on u.id = c.user_id
+order by e.experiment_name asc, c.revenue desc, u.username asc;
